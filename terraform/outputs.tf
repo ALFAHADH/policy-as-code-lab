a9a161bd3f483832175aadc3a501d0de6863fa4b
ALFAHADH <s.alfahadh.472@gmail.com>
2026-05-08 18:17:22 +0530

    refactor: replace custom OPA with framework policies (Regula + Terrascan) hjdjaFJ

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,6 +9,10 @@ output "bucket_name" {
 output "vm_name" {
   value = google_compute_instance.lab_vm.name
 }
+
+
+
+
 /*
 output "vm_public_ip" {
   value = google_compute_instance.lab_vm.network_interface[0].access_config[0].nat_ip
